eea1866f7c604108d901e8e68eb6796e40696684
EaZyXxX <ostanin.ol@yandex.ru>
2023-05-16 17:16:00 +0300

    Разбираемся с CORS

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
+const cors = require('cors');
 const index = require('./routes/index');
 
 mongoose.set('strictQuery', false);
@@ -18,6 +19,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.use(index);
 
