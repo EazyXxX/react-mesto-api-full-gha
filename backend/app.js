f76bf75ff5abdb23dae97aa6f867d3251df893d6
EaZyXxX <ostanin.ol@yandex.ru>
2023-05-16 18:08:41 +0300

    Разбираемся с CORS

--- backend/app.js
+++ backend/app.js
@@ -20,6 +20,10 @@ app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({ origin: 'https://eazyxxx.front.nomoredomains.monster' }));
+app.use(cors({ origin: 'https://eazyxxx.front.nomoredomains.monster/users/me' }));
+app.use(cors({ origin: 'https://eazyxxx.front.nomoredomains.monster/cards' }));
+
+
 
 app.use(index);
 
