d9e9426d7fd8e457e065d99076fd7ef985bfb95a
EaZyXxX <ostanin.ol@yandex.ru>
2023-05-16 18:04:50 +0300

    Разбираемся с CORS

--- backend/app.js
+++ backend/app.js
@@ -19,7 +19,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({ origin: 'https://eazyxxx.front.nomoredomains.monster' }));
 
 app.use(index);
 
