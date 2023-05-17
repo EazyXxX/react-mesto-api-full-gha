10c34d5cd09f4c58fdabb260d3db774b700b9d3f
EaZyXxX <ostanin.ol@yandex.ru>
2023-05-17 19:33:07 +0300

    test commit

--- backend/validation/validation.js
+++ backend/validation/validation.js
@@ -34,7 +34,7 @@ const updateUserProfileValidation = celebrate({
 
 const updateUserAvatarValidation = celebrate({
   body: Joi.object().keys({
-    avatar: Joi.string().required().pattern(/(https?:\/\/)(w{3}\.)?(((\d{1,3}\.){3}\d{1,3})|((\w-?)+\.(ru|com)))(:\d{2,5})?((\/.+)+)?\/?#?/),
+    avatar: Joi.string().required().pattern(/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/),
   }),
 });
 
