ed6763db8cc3d0da8bae7d08be356504be8d2900
EaZyXxX <ostanin.ol@yandex.ru>
2023-05-20 18:23:48 +0300

    test commit

--- backend/validation/validation.js
+++ backend/validation/validation.js
@@ -14,6 +14,7 @@ const signUpValidation = celebrate({
 const signInValidation = celebrate({
   body: Joi.object().keys({
     email: Joi.string().email().required(),
+    password: Joi.string().required(),
   }),
 });
 
