fed954c008bee1aeb3eb19ca81296a7fe8dcc872
EaZyXxX <ostanin.ol@yandex.ru>
2023-05-27 12:38:14 +0300

    test commit

--- backend/middlewares/centralCatcher.js
+++ backend/middlewares/centralCatcher.js
@@ -4,7 +4,7 @@ const centralCatcher = (err, req, res, next) => {
     return res.status(500).send({ message: 'Ошибка на стороне сервера' });
   }
   console.log(err);
-  res.status(err.statusCode).send(err.message);
+  res.status(err.statusCode).send({ message: err.message });
 };
 
 module.exports = centralCatcher;
